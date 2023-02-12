183e71a5f9f403cc6f01658c7200ab929bdd2efb
Kevin Do <do.kevin.vo@gmail.com>
2023-02-11 22:41:37 -0800

    :tada: adds Prisma to server; fixes ts config for main.ts; fixes Prisma client generation; set proper Node ver for Prisma; adds server doc

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,8 +1,46 @@
 import express from 'express';
+import { config } from 'dotenv';
+import { PrismaClient } from '../../../node_modules/.prisma/client/index.js';
+
+if (process.env.NODE_ENV !== 'production') {
+  config({
+    path: '.envrc',
+  });
+}
 
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 
 const app = express();
+
+const prisma = new PrismaClient();
+
+async function main() {
+  // await prisma.user.create({
+  //   data: {
+  //     email: 'user@example.com',
+  //     name: 'Alex Doe',
+  //     type: 'administrator',
+  //     password: 'password123',
+  //     apiKey: null,
+  //   },
+  // });
+  // const allUsers = await prisma.user.findMany({
+  //   include: {
+  //     images: true,
+  //   },
+  // });
+  // console.log('users: ', allUsers);
+}
+
+main()
+  .then()
+  .catch(async (error) => {
+    console.error(error);
+
+    await prisma.$disconnect();
+
+    process.exit(1);
+  });
 
 app.get('/', (req, res) => {
   res.send({ message: 'Hello API' });
