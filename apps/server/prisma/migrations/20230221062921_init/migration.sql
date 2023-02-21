e4389c0a3f8dc21fd9848c5cfd47d7aa15c083f2
Kevin Do <do.kevin.vo@gmail.com>
2023-02-20 22:34:41 -0800

    :sparkles: adds typegraphql & nanoid

--- apps/server/prisma/migrations/20230221062921_init/migration.sql
+++ apps/server/prisma/migrations/20230221062921_init/migration.sql
@@ -1,24 +1,30 @@
+-- CreateEnum
+CREATE TYPE "UserKind" AS ENUM ('NORMAL', 'ADMIN', 'GOD');
+
 -- CreateTable
 CREATE TABLE "Image" (
-    "id" SERIAL NOT NULL,
+    "id" TEXT NOT NULL DEFAULT nanoid(),
     "name" VARCHAR(255) NOT NULL,
     "createdAt" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "uploaderId" INTEGER NOT NULL,
-
-    CONSTRAINT "Image_pkey" PRIMARY KEY ("id")
+    "uploaderId" TEXT NOT NULL
 );
 
 -- CreateTable
 CREATE TABLE "User" (
-    "id" SERIAL NOT NULL,
+    "id" TEXT NOT NULL DEFAULT nanoid(),
     "email" VARCHAR(255) NOT NULL,
     "name" VARCHAR(255),
     "type" VARCHAR(255) NOT NULL,
     "password" VARCHAR(255),
     "apiKey" VARCHAR(255),
-
-    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+    "kind" "UserKind" NOT NULL
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Image_id_key" ON "Image"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_id_key" ON "User"("id");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
